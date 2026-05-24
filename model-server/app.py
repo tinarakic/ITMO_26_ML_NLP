4d710cc69e0ef8846207cfd8c1780c80f1768760
tinarakic <tina.rakic22@gmail.com>
2026-05-24 20:20:44 +0300

    Update app.py

--- model-server/app.py
+++ model-server/app.py
@@ -64,8 +64,7 @@ def generate(req: Request):
             top_p=0.85,
             repetition_penalty=1.12,
             no_repeat_ngram_size=3,
-            pad_token_id=tokenizer.eos_token_id,
-            eos_token_id=tokenizer.eos_token_id
+            pad_token_id=tokenizer.eos_token_id
         )
 
     generated_tokens = output[0][inputs["input_ids"].shape[-1]:]
