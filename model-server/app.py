f806eb521786eb59f1739afd5595fd6fd9f1bcc3
tinarakic <tina.rakic22@gmail.com>
2026-05-24 21:17:01 +0300

    Update app.py

--- model-server/app.py
+++ model-server/app.py
@@ -31,9 +31,12 @@ class Request(BaseModel):
 @app.post("/generate")
 def generate(req: Request):
 
-    inputs = tokenizer(req.prompt, return_tensors="pt")
+    inputs = tokenizer(
+    req.prompt,
+    return_tensors="pt"
+    )
+    
     inputs = {k: v.to(device) for k, v in inputs.items()}
-
     input_len = inputs["input_ids"].shape[1]
 
     with torch.no_grad():
@@ -48,9 +51,7 @@ def generate(req: Request):
             pad_token_id=tokenizer.eos_token_id
         )
 
-    # ONLY take generated tokens (not the prompt)
     generated_tokens = output[0][input_len:]
-
     text = tokenizer.decode(generated_tokens, skip_special_tokens=True)
 
     return {"text": text.strip()}
