f6af0dec935acf538a26db19e258b07b58717f20
tinarakic <tina.rakic22@gmail.com>
2026-05-24 16:51:09 +0300

    Add files via upload

--- model-server/app.py
+++ model-server/app.py
@@ -11,7 +11,8 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
     torch_dtype=torch.float16,
-    device_map="auto"
+    device_map="auto",
+    token=os.environ["HF_TOKEN"]
 )
 
 class Request(BaseModel):
